b1858bb244f5646435eeaab9660a4f80806940ef
kotaoue <kotaoue@gmail.com>
2020-09-21 22:50:37 +0900

    Add test for Card.URL().

--- packages/ygodb/ygodb_test.go
+++ packages/ygodb/ygodb_test.go
@@ -6,6 +6,15 @@ import (
 	"github.com/kotaoue/ygolinker/packages/ygodb"
 )
 
+func Test_Card_URL(t *testing.T) {
+	c := ygodb.Card{ID: "123456789"}
+	r := c.URL()
+	e := "https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid=123456789"
+	if r != e {
+		t.Fatalf("when Card.ID %s, returned %s, expected %s", c.ID, r, e)
+	}
+}
+
 func Test_ExtractValue(t *testing.T) {
 	s := "ATK 3000"
 	r := ygodb.ExtractValue(s)
