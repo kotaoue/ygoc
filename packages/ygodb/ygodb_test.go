0b7fc868e90e3bd1a73e6ab6b289ce95f50d87f5
kotaoue <kotaoue@gmail.com>
2020-09-22 09:04:16 +0900

    Fix Test_ExtractCardID.

--- packages/ygodb/ygodb_test.go
+++ packages/ygodb/ygodb_test.go
@@ -39,8 +39,8 @@ func Test_ExtractCardID(t *testing.T) {
 	}
 
 	s = "https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&keyword=Gouki+Suprex&stype=1&ctype=&starfr=&starto=&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=&atkto=&deffr=&defto=&othercon=2&request_locale=en"
-	r = ygodb.ExtractValue(s)
-	e = "1"
+	r = ygodb.ExtractCardID(s)
+	e = ""
 	if r != e {
 		t.Fatalf("when set %s, returned %s, expected %s", s, r, e)
 	}
